c00b3b36e3ffbbb6a83ee906bec2e9edd0a35d83
Nicolas Santana <nicolassantanadev@gmail.com>
2025-02-10 18:28:36 -0300

    Added chat message area: input field, send message button, and background styling.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Image from "./assets/foto.jpeg"
+import SendMessageIcon from "./assets/img-enviar.png"
 
 function App() {
   return (
@@ -17,6 +18,32 @@ function App() {
               </div>
             </div>
           </div>
+
+
+
+        <div className='chat-messages'>
+          <div className='chat-options'>
+           <div className='chat-item'>
+              <img src={Image} className='image-profile' alt='' />
+              <div className='title-chat-container'>
+                <span className='title-message'>Nicolas Santana </span>
+                <span className='last-message'></span>
+              </div>
+            </div>
+          </div>
+
+
+          <div className='chat-messages-area'>
+
+          </div>
+
+
+          <div className='chat-input-area'>
+            <input className='chat-input' placeholder='Mensagem'/>
+            <img src={SendMessageIcon} alt='' className='send-message-icon'/>
+          </div>
+        </div>
+
 
        </div>
     </div>
