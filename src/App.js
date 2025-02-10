bb86cb962f113af5bf10875b233a631a36dfc3dd
Nicolas Santana <nicolassantanadev@gmail.com>
2025-02-10 17:25:58 -0300

    Initial layout for WhatsApp Web clone: added chat container, contacts list, and basic styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
 import './App.css';
+import Image from "./assets/foto.jpeg"
 
 function App() {
   return (
-    <div className="">
-       a
+    <div className="container">
+       <div className='back-ground'></div>
+       <div className='chat-container'>
+        
+          <div className='chat-contacts'>
+            <div className='chat-options'></div>
+            <div className='chat-item'>
+              <img src={Image} className='image-profile' alt='' />
+              <div className='title-chat-container'>
+                <span className='title-message'>Nicolas Santana </span>
+                <span className='last-message'> Nicolas: Olá</span>
+              </div>
+            </div>
+          </div>
+
+       </div>
     </div>
   );
 }
